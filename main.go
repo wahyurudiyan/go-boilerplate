0488549b3ea3ac4ac8b4db0d82f8cbcc6f04e22f
Wahyu Rudiyan S <wahyurudiyansaputra@gmail.com>
2025-04-24 09:46:00 +0700

    [CHORE]: Enhance HTTP Server with graceful runner

--- main.go
+++ main.go
@@ -3,17 +3,31 @@ package main
 import (
 	"context"
 	"log/slog"
+	"time"
 
 	"github.com/wahyurudiyan/go-boilerplate/cmd/rest"
 	env "github.com/wahyurudiyan/go-boilerplate/config"
 	"github.com/wahyurudiyan/go-boilerplate/pkg/config"
+	"github.com/wahyurudiyan/go-boilerplate/pkg/graceful"
 )
 
 func main() {
 	var cfg env.ServiceConfig
 	config.Load(&cfg)
 
-	if err := rest.NewServer(&cfg).Listen(); err != nil {
-		slog.ErrorContext(context.Background(), "unable to run server", "error", err)
+	runApp := map[string]graceful.ExecCallback{
+		"http-server": func(ctx context.Context) (graceful.ShutdownCallback, error) {
+			srv := rest.NewServer(&cfg)
+			if err := srv.Listen(); err != nil {
+				slog.ErrorContext(context.Background(), "unable to run server", "error", err)
+				return nil, err
+			}
+
+			return func(ctx context.Context) error {
+				return srv.Shutdown(ctx)
+			}, nil
+		},
 	}
+
+	graceful.Runner(context.Background(), time.Duration(10*time.Second), runApp)
 }
